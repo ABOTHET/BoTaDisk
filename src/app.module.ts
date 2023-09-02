62646ca4427d54d2a84c304221b766130a9a2aa2
abothet <lolpoplolpop700@mail.ru>
2023-09-02 07:19:28 +0300

    Настроил проект, а именно: - Добавил ENV

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
+import { AccountsModule } from './accounts/accounts.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: "src/config/.env",
+      isGlobal: true,
+    }),
+    AccountsModule,
+  ],
   controllers: [],
   providers: [],
 })
